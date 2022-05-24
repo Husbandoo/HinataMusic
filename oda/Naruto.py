36ca4e850e5203701e9b5cc1dd402c06a4ab9bc3
Makima <105355430+Control-Devil@users.noreply.github.com>
2022-05-24 18:51:57 +0530

    Delete Naruto.py

--- oda/Naruto.py
+++
@@ -1,6 +0,0 @@
-PM_START_PIC = "https://telegra.ph/file/92c305c99bde53b6378f1.jpg"
-
-PM_START_TEXT = f"""<b>✨ Welcome {message.from_user.first_name} - San!
-        I am [{BOT_NAME}](https://t.me/{BOT_USERNAME}) Group Music Bot , Which Can Play Songs In Your Group Voice Chat In Easy Way
-        
-❓ Find Out All The Bot's Commands & How They Work By Clicking On The ➤ /help</b>"""
